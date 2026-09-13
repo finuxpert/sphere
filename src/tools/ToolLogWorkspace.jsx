436cc503282315bc47b00d23970ccc3d10d3ec08
Zactos-lab <fikrirtc12@gmail.com>
2026-09-13 17:25:08 +0700

    style: load final Rundeck workspace polish

--- src/tools/ToolLogWorkspace.jsx
+++ src/tools/ToolLogWorkspace.jsx
@@ -5,6 +5,7 @@ import './SphereWorkspaceV155.css'
 import './SphereWorkspaceV156.css'
 import './components/RundeckOperationalClarity.css'
 import './components/RundeckWorkspaceCompact.css'
+import './components/RundeckWorkspaceFinalPolish.css'
 
 const LogAutoSphereV5 = React.lazy(() => import('./ToolLogAutoSphereV5.jsx'))
 
