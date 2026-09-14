072bd8da91d49a179007b15fe09f7ca8dff6b96d
Zactos-lab <fikrirtc12@gmail.com>
2026-09-14 13:16:57 +0700

    refactor: switch monitoring core to canonical component imports

--- src/tools/components/RundeckMonitoringHistoryCore.jsx
+++ src/tools/components/RundeckMonitoringHistoryCore.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
-import RundeckAppServersV1234 from './RundeckAppServersV1234.jsx'
+import RundeckAppServers from './RundeckAppServers.jsx'
 import RundeckJobHistory from './RundeckJobHistory.jsx'
-import RundeckObservationHistoryV1234 from './RundeckObservationHistoryV1234.jsx'
-import RundeckServerTrendV1234 from './RundeckServerTrendV1234.jsx'
+import RundeckObservationHistory from './RundeckObservationHistory.jsx'
+import RundeckServerTrend from './RundeckServerTrend.jsx'
 
 export default function RundeckMonitoringHistoryCore(props) {
   return <section className="rundeckMonitoring rundeckMonitoringV1234 rundeckMonitoringV1235">
     <section className="rundeckServerTrendBandV1234 rundeckServerTrendBandV1235">
       <div className="rundeckBandPaneV1234 rundeckBandPaneV1235 is-app-servers">
-        <RundeckAppServersV1234 refreshToken={props.refreshToken} latestCollectionId={props.latestCollectionId} onSelectJob={props.onSelectJob} />
+        <RundeckAppServers refreshToken={props.refreshToken} latestCollectionId={props.latestCollectionId} onSelectJob={props.onSelectJob} />
       </div>
       <div className="rundeckBandPaneV1234 rundeckBandPaneV1235 is-server-trend">
-        <RundeckServerTrendV1234 refreshToken={props.refreshToken} databaseEnabled={props.databaseEnabled} selectedJob={props.selectedJob} onSelectJob={props.onSelectJob} onTrendContext={props.onTrendContext} />
+        <RundeckServerTrend refreshToken={props.refreshToken} databaseEnabled={props.databaseEnabled} selectedJob={props.selectedJob} onSelectJob={props.onSelectJob} onTrendContext={props.onTrendContext} />
       </div>
     </section>
     <section className="rundeckWorkloadBandV1234 rundeckWorkloadBandV1235">
@@ -20,6 +20,6 @@ export default function RundeckMonitoringHistoryCore(props) {
         <RundeckJobHistory job={props.selectedJob} refreshToken={props.refreshToken} incidentStart={props.incidentStart} latestCollectionId={props.latestCollectionId} />
       </div>
     </section>
-    <RundeckObservationHistoryV1234 job={props.selectedJob} refreshToken={props.refreshToken} />
+    <RundeckObservationHistory job={props.selectedJob} refreshToken={props.refreshToken} />
   </section>
 }
